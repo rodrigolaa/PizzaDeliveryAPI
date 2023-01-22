5f78f8fb6f8b2d594685a5849d5da0a8fc42e190
Rodrigo Lacerda <rodrigo.lacerda@rappi.com>
2023-01-22 02:00:06 -0300

    Pizza delivery image

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import declarative_base,sessionmaker
 
-engine = create_engine('postgresql://postgres:gdfather.1@localhost/pizzadelivery',
-
+engine = create_engine(
+'postgresql://postgres:gdfather.1@localhost/pizzadelivery',
 echo=True
-
 )
 
 Base = declarative_base()
